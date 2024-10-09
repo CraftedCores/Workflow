0504f7bb8afd8d8c88c04125843b06822f18565f
CraftedCores <101159375+CraftedCores@users.noreply.github.com>
2024-10-09 14:48:53 -0600

    Project files

---
+++ Workflow/WorkflowTests/WorkflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowTests.swift
+//  WorkflowTests
+//
+//  Created by Chandler Johnson on 10/9/24.
+//
+
+import Testing
+@testable import Workflow
+
+struct WorkflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
